ba2d4671866a76a28f90dae850f4d440fc09ab84
Ashkan Yarmoradi <ayarmoradi@outlook.com>
2021-10-14 00:59:09 +0300

    Catch Exception when throw rabbitmq is not ready

--- src/Ericsson.Client/Ericsson.Client/Worker.cs
+++ src/Ericsson.Client/Ericsson.Client/Worker.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
+using RabbitMQ.Client.Exceptions;
 
 namespace Ericsson.Client
 {
@@ -32,23 +33,38 @@ namespace Ericsson.Client
             };
         }
 
-        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            // Throw Exception if cancel request happened
+            Start:
+            
             stoppingToken.ThrowIfCancellationRequested();
 
-            // Create Connection
-            _connection = _connectionFactory.CreateConnection();
+            await Task.Delay(1000);
+            
+            try
+            {
+                _logger.LogInformation("Try to connect to RabbitMq");
+
+                // Create Connection
+                _connection = _connectionFactory.CreateConnection();
+
+                _logger.LogInformation("Connection established to RabbitMq");
+            }
+            catch (BrokerUnreachableException)
+            {
+                _logger.LogInformation("Can't connect to RabbitMq");
+                goto Start;
+            }
 
             // Create Model
             _channel = _connection.CreateModel();
-            
+
             // Create Exchange Declare If Not Exist
             _channel.ExchangeDeclare(exchange: _rabbitMqOptions.Value.Exchange, type: ExchangeType.Fanout);
 
             // Create Queue With Random Name
             var queueName = _channel.QueueDeclare().QueueName;
-            
+
             // Bind Queue to Exchange
             _channel.QueueBind(queue: queueName,
                 exchange: _rabbitMqOptions.Value.Exchange,
@@ -56,28 +72,33 @@ namespace Ericsson.Client
 
             // Create Basic Event Consumer
             var consumer = new EventingBasicConsumer(_channel);
-            
+
             consumer.Received += (_, ea) =>
             {
                 var body = ea.Body.ToArray();
                 var message = Encoding.UTF8.GetString(body);
-                Console.WriteLine(" [x] {0}", message);
                 _logger.LogInformation($" [x] {message}");
             };
-            
+
             // Consume
             _channel.BasicConsume(
                 queue: queueName,
                 autoAck: true,
                 consumer: consumer);
-            
-            return Task.CompletedTask;
         }
 
         public override void Dispose()
         {
-            _channel.Close();
-            _connection.Close();
+            if (_channel is null == false)
+            {
+                _channel.Close();
+            }
+
+            if (_channel is null == false)
+            {
+                _connection.Close();
+            }
+
             base.Dispose();
         }
     }
